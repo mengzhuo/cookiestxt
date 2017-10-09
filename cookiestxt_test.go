f4f3b3d6dc92c3b1f707a35490f6bd0a94796885
Meng Zhuo <mengzhuo1203@gmail.com>
2017-10-09 19:30:12 +0800

    init

---
+++ cookiestxt_test.go
@@ -0,0 +1,72 @@
+package cookiestxt
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestParseLine(t *testing.T) {
+	bt := time.Unix(946684799, 0)
+	c, err := ParseLine(".netscape.com TRUE / TRUE 946684799 NETSCAPE_ID 100103")
+	if err != nil || c.Value != "100103" || c.Expires.Before(bt) || !c.Secure {
+		t.Error(err)
+	}
+	t.Log(c)
+	_, err = ParseLine(".netscape.com / FALSE 946684799 NETSCAPE_ID 100103")
+	if err == nil {
+		t.Error("no error on invalid txt")
+	}
+}
+
+func TestParseHTTPOnlyLine(t *testing.T) {
+	c, err := ParseLine("#HttpOnly_.netscape.com TRUE / FALSE 946684799 NETSCAPE_ID 100103")
+	if err != nil || c.Domain != ".netscape.com" || c.HttpOnly == false {
+		t.Error(err)
+	}
+	t.Log(c)
+}
+
+func TestParseUnixTime(t *testing.T) {
+	c, err := ParseLine("#HttpOnly_.netscape.com TRUE / FALSE -1 NETSCAPE_ID 100103")
+	if err != nil || c.Expires.After(time.Unix(0, 0)) {
+		t.Error(err)
+	}
+	t.Log(c)
+
+	_, err = ParseLine("#HttpOnly_.netscape.com TRUE / FALSE NOT_A_INT NETSCAPE_ID 100103")
+	if err == nil {
+		t.Error("no error on invalid expires")
+	}
+}
+
+func TestParseFunc(t *testing.T) {
+	mock := `
+	# Comment
+	# More Comment
+	# This is a long comment that_has_7 fields
+
+	#not very well comment
+	#HttpOnly_.netscape.com TRUE / FALSE 946684799 NETSCAPE_ID 100103
+	.netscape.com TRUE / FALSE 946684799 NETSCAPE_ID 100103
+	#
+
+	`
+	cl, err := Parse(strings.NewReader(mock))
+	if err != nil || len(cl) != 2 {
+		t.Error(err, cl)
+	}
+}
+
+func TestParseFailed(t *testing.T) {
+	mock := `
+	#
+	#HttpOnly_.netscape.com TRUE / FALSE NOT_A_INT NETSCAPE_ID 100103
+	#
+
+	`
+	_, err := Parse(strings.NewReader(mock))
+	if err == nil || strings.Index(err.Error(), "line:3") == -1 {
+		t.Error(err)
+	}
+}
