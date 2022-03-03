360eb450f2dbd89659349af32c88d2aa25e1239e
Joseph Vusich <15684831+josephvusich@users.noreply.github.com>
2022-03-03 19:37:43 +0000

    fix: handle cookies with an empty value
    strings.Fields omits the empty trailing field on cookies with
    a blank value.

--- cookiestxt.go
+++ cookiestxt.go
@@ -74,7 +74,9 @@ func Parse(rd io.Reader) (cl []*http.Cookie, err error) {
 // ParseLine parse single cookie from one line
 func ParseLine(raw string) (c *http.Cookie, err error) {
 	f := strings.Fields(raw)
-	if len(f) < fieldsCount {
+	if len(f) == fieldsCount-1 {
+		f = append(f, "")
+	} else if len(f) < fieldsCount {
 		err = fmt.Errorf("expecting fields=7, got=%d", len(f))
 		return
 	}
